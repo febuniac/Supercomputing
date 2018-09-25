c3139c1c487bd963894165dae3f53a2e30766341
Felipe Buniac <febuniac@gmail.com>
2018-09-24 23:59:22 -0300

    1314.1

--- 13-14-sincronizacao/prod_cons.cpp
+++ 13-14-sincronizacao/prod_cons.cpp
@@ -13,7 +13,7 @@ std::mutex mutexs;
 void produce(queue<int>& q, semaphore& s){
 
 	int it =rand()%101;;
-	std::unique_lock<std::mutex> lk(mutexs);
+	//std::unique_lock<std::mutex> lk(mutexs);
     q.push(it);
 	cout << "Produz: " << it << "\n";
 	this_thread::sleep_for(std::chrono::milliseconds(1000));
@@ -26,19 +26,20 @@ void consume(queue<int>& q, semaphore& s){
 
 	s.acquire();
 	cout << "Consome: " <<pow(q.front(),2) << "\n";
-	std::unique_lock<std::mutex> lk(mutexs);
+	//std::unique_lock<std::mutex> lk(mutexs);
+    this_thread::sleep_for(std::chrono::milliseconds(500));
     q.pop();	
-	this_thread::sleep_for(std::chrono::milliseconds(300));
 	
     consume(q,s);
 
 }
 
 int main(){
-
+    srand (time(NULL));
 	queue<int> q;
 
 	semaphore s(0);
+
 
 	thread t1(produce,ref(q),ref(s));
 	thread t2(consume,ref(q),ref(s));
